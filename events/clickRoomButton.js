bc18b65e443402a615f906df3075cf122ea7928f
Romanov Roman <76689339+HekaHub@users.noreply.github.com>
2022-07-15 10:54:09 +0700

    Update clickRoomButton.js

--- events/clickRoomButton.js
+++ events/clickRoomButton.js
@@ -22,7 +22,7 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 if(m.content.length > 20) m.content = m.content.slice(0, 20)
 
                 room.cooldown = Math.round(Date.now() + (60*5*1000))
@@ -39,9 +39,9 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 if(isNaN(m.content) || 0 > m.content || m.content > 99) return button.editReply({content: `Укажите число не больше **99**. Если вы укажите 0, то лимит изменится на безграничное число присоеденений`})
-                channel.edit({userLimit: m.content})
+                await channel.edit({userLimit: m.content})
 
                 return button.editReply({content: `Вы изменили лимит канала на **${m.content}**`})
             })
@@ -56,13 +56,13 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 let member = getMember(m, m.content)
                 if(!member || member.id == button.user.id || member.user.bot) return button.editReply({content: `Участник не был найден`, ephemeral: true})
                 if(!member?.voice?.channel) return button.editReply({content: `Участник находится не в голосовом канале`, ephemeral: true})
                 if(member.voice.channel.id !== room.channelId) return button.editReply({content: "Участник находится не в вашем приватном канале", ephemeral: true})
 
-                member.voice.disconnect()
+                await member.voice.disconnect().catch(() => {})
                 return button.editReply({content: `Участник <@!${member.id}> был выгнан с комнаты`, ephemeral: true})
             })
 
@@ -92,11 +92,11 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 let member = getMember(m, m.content)
                 if(!member || member.id == button.user.id || member.user.bot) return button.editReply({content: `Участник не был найден`, ephemeral: true})
 
-                channel.permissionOverwrites.create(member.id, {CONNECT: true})
+                await channel.permissionOverwrites.create(member.id, {CONNECT: true})
                 return button.editReply({content: `Участник <@!${member.id}> теперь **может** присоеденяться к **вашей** комнате`, ephemeral: true})
             })
         } else if(button.customId == 'pvremoveuser') {
@@ -104,11 +104,11 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 let member = getMember(m, m.content)
                 if(!member || member.id == button.user.id || member.user.bot) return button.editReply({content: `Участник не был найден`, ephemeral: true})
 
-                channel.permissionOverwrites.create(member.id, {CONNECT: false})
+                await channel.permissionOverwrites.create(member.id, {CONNECT: false})
                 return button.editReply({content: `Участник <@!${member.id}> больше **не может** присоеденяться к **вашей** комнате`, ephemeral: true})
             })
         } else if(button.customId == 'pvmute') {
@@ -116,11 +116,11 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 let member = getMember(m, m.content)
                 if(!member || member.id == button.user.id || member.user.bot) return button.editReply({content: `Участник не был найден`, ephemeral: true})
 
-                channel.permissionOverwrites.create(member.id, {SPEAK: false})
+                await channel.permissionOverwrites.create(member.id, {SPEAK: false})
                 return button.editReply({content: `Вы **закрыли** ротик участнику <@!${member.id}>`, ephemeral: true})
             })
         } else if(button.customId == 'pvunmute') {
@@ -128,11 +128,11 @@ class ClickRoomButton extends Event {
             const filter = m => m.author.id === button.user.id
             const collector = button.channel.createMessageCollector({filter: filter, time: 30000, max: 1 })
 
-            collector.on('collect', m => {
+            collector.on('collect', async m => {
                 let member = getMember(m, m.content)
                 if(!member || member.id == button.user.id || member.user.bot) return button.editReply({content: `Участник не был найден`, ephemeral: true})
 
-                channel.permissionOverwrites.create(member.id, {SPEAK: true})
+                await channel.permissionOverwrites.create(member.id, {SPEAK: true})
                 return button.editReply({content: `Вы **открыли** ротик участнику <@!${member.id}>`, ephemeral: true})
             })
         }
