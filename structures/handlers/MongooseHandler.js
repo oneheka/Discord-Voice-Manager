0fcdb11757010151b09da4bc7dd48f37b1c68eb8
Romanov Roman <76689339+HekaHub@users.noreply.github.com>
2022-07-25 15:26:27 +0700

    Delete structures directory

--- structures/handlers/MongooseHandler.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-module.exports = (bot) => {
-    mongoose.connect(bot.config.internal.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log(`База данных подключена.`)
-    })
-    .catch(() => {})
-}
