bb38394d0c815369368b33990516763503eb7b7c
Romanov Roman <76689339+HekaHub@users.noreply.github.com>
2022-07-25 15:26:15 +0700

    Delete functions directory

--- functions/getChannel.js
+++
@@ -1,6 +0,0 @@
-module.exports = (message, channel, options = {}) => {
-    let Channel = message.guild.channels.cache.find(c => c.name === channel)
-    if(!Channel) Channel = message.guild.channels.cache.get(channel)
-    if(!Channel && !options) Channel = message.mentions.channels.first()
-    return Channel
-}
