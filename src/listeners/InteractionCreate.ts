2330d69c2a50a026c6e49e9b2d2ed4e1fb9f1ef4
Romanov Roman <76689339+HekaHub@users.noreply.github.com>
2022-07-26 11:27:09 +0700

    Update InteractionCreate.ts

--- src/listeners/InteractionCreate.ts
+++ src/listeners/InteractionCreate.ts
@@ -13,7 +13,7 @@ export default new Event(
             const author = interaction.member as GuildMember
             const room = await Room.findOne({guildId: button.guild.id, userId: author.id})
 
-            if(!room || !author?.voice || !author?.voice?.channel) return button.reply({content: 'Вы находитесь **не в голосовом** канале', ephemeral: true})
+            if(!room || !author?.voice?.channel) return button.reply({content: 'Вы находитесь **не в голосовом** канале', ephemeral: true})
             let channel = interaction.guild.channels.cache.get(room.channelId) as VoiceChannel
     
             if(!channel) await room.remove()
@@ -22,8 +22,7 @@ export default new Event(
             if(author.voice.channel.id !== room.channelId) return button.reply({content: 'Зайдите в **свой** приватный канал', ephemeral: true})
 
             if(bot.buttons.has(button.customId)) {
-                await bot.buttons.get(button.customId)(button, room, channel)
-                return
+                return bot.buttons.get(button.customId)(button, room, channel)
             } else {
                 if(!button.deferred) await button.deferUpdate().catch(() => {})
                 return
