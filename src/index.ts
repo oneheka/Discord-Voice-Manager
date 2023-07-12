d3a64870652820926910be099e586716fa04543a
Mihail Litvin <76689339+HekaHub@users.noreply.github.com>
2023-07-12 21:06:47 +0700

    Delete src directory

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-console.clear()
-import Client from './strcut/Client';
-new Client().start()
--- src/strcut/base/Event.ts
+++
@@ -1,8 +0,0 @@
-import { EventOptions, EventRun } from '../../types/base/Event';
-
-export default class Event {
-    constructor(
-        public options: EventOptions,
-        public run: EventRun
-    ) {}
-}
